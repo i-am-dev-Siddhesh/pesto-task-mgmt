a2be1445695759ca73c1ad502ef722bf9eb51657
siddhesh shinde <shindesiddhesh.co.in@gmail.com>
2024-01-26 22:12:54 +0530

    feat: fetch worker in front end implemented

--- pesto-service/dist/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = void 0;
-const url_1 = require("url");
-const test = (url) => {
-    return new url_1.URL(url).pathname;
-};
-exports.test = test;
